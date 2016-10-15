7211ac8cd2ed09a112abfed9b2fb9e0e38cc2e4b
Christian Smith <cesm@umich.edu>
2016-10-14 21:04:02 -0400

    Added GameManager class

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+  private static GameManager _instance;
+  public static GameManager Instance {
+    get {
+      return _instance;
+    }
+  }
+
+  private void Awake() {
+    if (_instance != null && _instance != this) {
+      Destroy(this.gameObject);
+    }
+    else {
+      _instance = this;
+    }
+  }
+
+}
