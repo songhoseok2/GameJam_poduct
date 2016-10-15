2787449f49aee2881f02a27cf3c2115837782f16
Christian Smith <cesm@umich.edu>
2016-10-14 21:25:24 -0400

    Rewrote GameManager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,22 +4,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-
-  private static GameManager _instance;
-  public static GameManager Instance {
-    get {
-      return _instance;
-    }
-  }
-
-  private void Awake() {
-    if (_instance != null && _instance != this) {
-      Destroy(this.gameObject);
-    }
-    else {
-      _instance = this;
-    }
-  }
-
+public class GameManager : MonoBehaviour
+{
+	public int resources;
 }
