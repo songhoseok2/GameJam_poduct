b9969c7f85387d2e3604dfd2217f3e926e70b532
Christian Smith <cesm@umich.edu>
2016-10-14 21:44:34 -0400

    Implemented unit selection

---
+++ Assets/Components/Selectable.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Selectable : MonoBehaviour
+{
+    public GameObject selectionCircle;
+}
