0fa8aeff83a450210fc601c9c8d21d27f27513d0
Ho Seok Song <hoseok@umich.edu>
2016-10-15 14:05:01 -0400

    wrote spawning scripts for troop and enemy

---
+++ Assets/Components/SpawnEnemies.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject EnemyToSpawn;
+
+
+    public float spawnInterval;
+    private float next_spawn;
+
+    IEnumerator wait(int seconds)
+    {
+        yield return new WaitForSecondsRealtime(seconds);
+    }
+    
+    // Use this for initialization
+    void Start ()
+    {
+        next_spawn = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Time.time >= next_spawn)
+        {
+            Instantiate(EnemyToSpawn,
+                new Vector3(Random.Range(transform.position.x - 5, transform.position.x + 5), Random.Range(transform.position.y - 5, transform.position.y + 5), transform.position.z),
+                Quaternion.identity);
+
+            next_spawn = Time.time + spawnInterval;
+        }
+
+        wait(5);
+    }
+}
---
+++ Assets/Components/Spawn_troops.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Spawn_troops : MonoBehaviour
+{
+    public GameObject troop_to_spawn;
+    public Text text_not_enough_resource;
+    char spawn_where = 'n';
+
+    //Utils.GameManager().resources
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        text_not_enough_resource.enabled = false;
+    }
+
+    IEnumerator display_resources_message()
+    {
+        text_not_enough_resource.enabled = true;
+        yield return new WaitForSecondsRealtime(2);
+        text_not_enough_resource.enabled = false;
+    }
+
+
+
+    void OnMouseDown()
+    {
+        if (Utils.GameManager().resources >= 10)//enough resource
+        {
+            switch (spawn_where)
+            {
+                case 'n':
+                    Instantiate(troop_to_spawn, transform.position + new Vector3(0, 5, -10), transform.rotation);
+                    spawn_where = 'e';
+                    break;
+                case 'e':
+                    Instantiate(troop_to_spawn, transform.position + new Vector3(5, 0, -10), transform.rotation);
+                    spawn_where = 'w';
+                    break;
+                case 'w':
+                    Instantiate(troop_to_spawn, transform.position + new Vector3(-5, 0, -10), transform.rotation);
+                    spawn_where = 's';
+                    break;
+                case 's':
+                    Instantiate(troop_to_spawn, transform.position + new Vector3(0, -5, -10), transform.rotation);
+                    spawn_where = 'n';
+                    break;
+            }
+
+            Utils.GameManager().resources = Utils.GameManager().resources - 200;
+        }
+
+        else
+        {
+            StartCoroutine(display_resources_message());
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+    }
+}
