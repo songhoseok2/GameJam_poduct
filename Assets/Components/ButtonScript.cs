1c89512ffe7118bc91c57acfdf088d48eda410de
Ho Seok Song <hoseok@umich.edu>
2016-10-15 20:44:29 -0400

    Added ButtonScript

---
+++ Assets/Components/ButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour
+{
+    public char buttonID;
+    // Use this for initialization
+	
+	public void clicked()
+    {
+        Utils.GameManager().SetSelection(buttonID);
+    }
+
+}
