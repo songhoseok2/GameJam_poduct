4e0b459e9da1c8df8aea9a3002499e9bc4b3a4d8
songhoseok2 <hoseok@umich.edu>
2017-01-26 15:36:30 -0500

    .exe added
    added .exe

---
+++ game in Standalone exe format/PeasantDestroyer_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,77 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CANNON = 2393348022U;
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID ERROR = 1880695829U;
+        static const AkUniqueID INITIALIZEMUSIC = 1640319276U;
+        static const AkUniqueID PLAYERMOVE = 2267600265U;
+        static const AkUniqueID PLAYSWORDS = 1264945005U;
+        static const AkUniqueID PURCHASE = 3809213376U;
+        static const AkUniqueID SET_GAME_PARAMETER = 1402181228U;
+        static const AkUniqueID SET_STATE = 2596512161U;
+        static const AkUniqueID TRAP = 3254962040U;
+        static const AkUniqueID WALLCRUMBLE = 4210596639U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace STATES
+        {
+            static const AkUniqueID GROUP = 1208172739U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BATTLE = 2937832959U;
+                static const AkUniqueID LOSE = 221232726U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID PREPARE = 2727857118U;
+            } // namespace STATE
+        } // namespace STATES
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID INTENSITY = 2470328564U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+        static const AkUniqueID SOUNDBANK2 = 1647770722U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
